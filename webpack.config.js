cabb834df9379c0f1e1cdc78d384add349d70403
Octosa Ryan <octosa@transdigital.co.id>
2020-11-10 10:55:51 +0700

    add extra config and dependency

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,35 @@
-const HTMLWebPackPlugin = require('html-webpack-plugin');
-module.exports = {  
-  module:{
-    rules:[
+const HTMLWebPackPlugin = require("html-webpack-plugin");
+const LinkTypePlugin = require("html-webpack-link-type-plugin")
+  .HtmlWebpackLinkTypePlugin;
+
+module.exports = {
+  module: {
+    rules: [
       {
-          test:/\.(js|jsx)$/,
-          exclude:/node_modules/,
-          use: {
-              loader:'babel-loader',
-          }
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
       },
+      {
+        test: /\.scss$/,
+        use: ["sass-loader"]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
     ]
   },
   plugins: [
     new HTMLWebPackPlugin({
-      template: './src/index.html',
-      filename: './index.html'
-      }
-    )
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new LinkTypePlugin({
+      "*.css": "text/css",
+      "*.js": "text/javascript"
+    })
   ]
 };
